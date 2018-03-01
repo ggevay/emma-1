490a335e5da0d4492d6d9af067417317b06bf425
Lorand Madai-Tahy <lorand.madai@gmail.com>
2018-03-01 16:31:39 +0100

    added emma compiler test structure

---
+++ emma-labyrinth/src/test/scala/org/emmalanguage/compiler/LabyrinthCompilerSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2014 TU Berlin (emma@dima.tu-berlin.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.emmalanguage
+package compiler
+
+//import api.DataBag
+
+class LabyrinthCompilerSpec extends BaseCompilerSpec
+  with LabyrinthCompilerAware
+  with LabyrinthAware {
+
+  override val compiler = new RuntimeCompiler(codegenDir) with LabyrinthCompiler
+
+  import compiler._
+  import u.reify
+
+  def withBackendContext[T](f: Env => T): T =
+    withDefaultFlinkStreamEnv(f)
+
+
+  val anfPipeline: u.Expr[Any] => u.Tree =
+    pipeline(typeCheck = true)(
+      Core.anf,
+      Core.unnest
+    ).compose(_.tree)
+
+  def applyXfrm(xfrm: Xfrm): u.Expr[Any] => u.Tree = {
+
+    pipeline(typeCheck = true)(
+      Core.lnf,
+      xfrm.timed,
+      Core.unnest
+    ).compose(_.tree)
+  }
+
+  // ---------------------------------------------------------------------------
+  // Spec tests
+  // ---------------------------------------------------------------------------
+
+  "test simple" in {
+    val inp = reify { val a = 1; a }
+    // val exp = reify { val a = DataBag(Seq(1)); a }
+    val exp = reify { val a = 1; a }
+
+    applyXfrm(nonbag2bag)(inp) shouldBe alphaEqTo(anfPipeline(exp))
+  }
+}
